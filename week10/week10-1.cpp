40b09367885333e7ef9ac960702790ba719bf54f
Yuan Yuan <91170703+mian9116@users.noreply.github.com>
2022-04-28 17:53:17 +0800

    Add files via upload

---
+++ week10/week10-1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+char name[100][300];
+int grade[100];
+
+int main()
+{
+    FILE*fin=fopen("input.txt","r");
+
+    int N;
+
+    fscanf(fin,"%d",&N);
+
+    for(int i=0;i<N;i++)
+    {
+        fscanf(fin,"%s%d",name[i],&grade[i]);
+    }
+
+    for(int i=0;i<N;i++)
+    {
+        printf("Åª¨ì¤F%s %d\n",name[i],grade[i]);
+    }
+}
